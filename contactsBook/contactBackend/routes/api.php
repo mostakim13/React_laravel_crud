53dd7dce708ab280e1174804cdee60630ae6b1c4
Mostakim <mimrzs2013@gmail.com>
2022-02-15 20:06:51 +0600

    react laravel crud

---
+++ contactsBook/contactBackend/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\API\ContactController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('contacts', [ContactController::class, 'index']);
+Route::post('/add-contact', [ContactController::class, 'store']);
+Route::get('/edit-contact/{id}', [ContactController::class, 'edit']);
+Route::put('update-contact/{id}', [ContactController::class, 'update']);
+Route::delete('delete-contact/{id}', [ContactController::class, 'destroy']);
+
+// Search Route
+Route::get('/search/{key}',[ContactController::class, 'contactBySearch']);
